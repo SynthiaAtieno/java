e4b11bd57226f9f9500f785da831ab98191e15c9
SynthiaOndeng <ondengsynthia222@gmail.com>
2023-06-13 23:04:48 +0300

    created the main class

--- test.java
+++ test.java
@@ -1,7 +1,11 @@
-
-    public static void fizzBuzz(int n) {
+class HelloWorld {
+    public static void main(String[] args) {
+        //System.out.println("Hello, World!");
+        fizzBuzz(30);
+    }
+       public static void fizzBuzz(int n) {
     // Write your code here
-    for(int i=1; i<n; i++){
+    for(int i=1; i<=n; i++){
         if(i%3==0 && i%5==0){
             System.out.println("FizzBuzz");
         }else if(i%3==0 && i%5!=0){
@@ -12,5 +16,6 @@
             System.out.println(i);
         }
     }
+    //return;
     }
-    fizzBuzz(15);
+}
